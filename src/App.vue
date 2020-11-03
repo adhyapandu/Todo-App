9dcbd977d69e0f5451bf6281789351217767b380
adhya28 <adhyapandu28@gmail.com>
2020-11-03 03:34:10 -0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <CompletedTodos></CompletedTodos>
+    <GetTodo></GetTodo>
+    <CurrentTodos></CurrentTodos>
+  </div>
+</template>
+
+<script>
+import GetTodo from './components/getTodo.vue'
+import CurrentTodos from './components/currentTodo.vue'
+import CompletedTodos from './components/completedTodo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    GetTodo,
+    CurrentTodos,
+    CompletedTodos
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
